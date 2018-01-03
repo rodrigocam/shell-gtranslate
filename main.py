106c70f64a33d8cb5ec6d8ecfa870dd3a08cc3ed
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2018-01-03 06:49:32 -0200

    First functional features

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+from api_connection import translate
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('text', type=str, help='text to translate')
+parser.add_argument('-s', '--src', default=None, help='origin language of the text')
+parser.add_argument('-d', '--dest', default=None, help='destiny language of the translation')
+parser.add_argument('-v', '--verbose', help='show more information', action='store_true')
+
+args = parser.parse_args()
+
+tr = translate(args.text, args.src, args.dest)
+
+if args.verbose:
+    print('original text: %s' % tr.origin)
+    print('translated text: %s' % tr.text)
+    print('origin language: %s' % tr.src)
+    print('destiny language: %s' % tr.dest)
+else:
+    print(tr.text)
